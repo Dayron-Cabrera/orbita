0c475d9d0b04b59832679d547ca4cbe74d205f95
shiftybody <cacdcbtis75@gmail.com>
2019-11-18 13:59:18 -0600

    en el nombre de dios

--- loT/orbita/orbita.ino
+++ loT/orbita/orbita.ino
@@ -1,48 +1,138 @@
 
+#include <ESP8266WiFi.h>     //librerias  para el mcu
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
 #include <SPI.h>
-#include <MFRC522.h>
+#include <MFRC522.h>        //libreria para el lector rfid
 
+#define SS_PIN D8 //conexiones de datos para la libreria
+#define RST_PIN D3
 
-#define SS_PIN D4
-#define RST_PIN D2
-const int pinBuzzer = D1;
+#define RedLed D1 // pines de la luz
+#define BlueLed D2
+
+const int pinBuzzer = D4; //pin para el buzzer 
 int b[5] = {247, 494, 988, 1976, 3951};
-int rojo = D0;
-int verde = D8;
 
-MFRC522 mfrc522(SS_PIN, RST_PIN); // Instance of the class
 
+MFRC522 mfrc522(SS_PIN, RST_PIN); // instanciamos el modulo rfid
+
+const char *ssid = "RT-AC1200_68_2G";  //ENTER YOUR WIFI SETTINGS
+const char *password = "agency_2297";
+
+//Web/Server address to read/write from 
+const char *host = "192.168.x.x";   //IP address of server
+
+String getData ,Link;
+String CardID="";
 
 void setup() {
+  delay(1000);
   Serial.begin(115200);
-  SPI.begin();       // Init SPI bus
-  mfrc522.PCD_Init(); // Init MFRC522
-  pinMode(rojo, OUTPUT);
-  pinMode(verde, OUTPUT);
+  SPI.begin();  // Init SPI bus
+  mfrc522.PCD_Init(); // Init MFRC522 card
 
+  WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
+  delay(1000);
+  WiFi.mode(WIFI_STA);        //This line hides the viewing of ESP as wifi hotspot
+  
+  WiFi.begin(ssid, password);     //Connect to your WiFi router
+  Serial.println("");
+
+  Serial.print("Conectando a: ");
+  Serial.print(ssid);
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.println("Conectado");
+  Serial.print("Direccion IP: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+
+  pinMode(RedLed,OUTPUT);
+  pinMode(BlueLed,OUTPUT);
+  
 }
 
 void loop() {
+  if(WiFi.status() != WL_CONNECTED){
+    WiFi.disconnect();
+    WiFi.mode(WIFI_STA);
+    Serial.print("Reconectando a:");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
 
-  if ( mfrc522.PICC_IsNewCardPresent())
-
-  {
-    if ( mfrc522.PICC_ReadCardSerial())
-    {
-      Serial.print("Tag UID:");
-      for (byte i = 0; i < mfrc522.uid.size; i++) {
-        Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-        Serial.print(mfrc522.uid.uidByte[i], HEX);
-      }
-      tone(pinBuzzer, b[5], 1000);
-      digitalWrite(verde, HIGH);
-      digitalWrite(rojo, LOW);
-      delay(250);
-      digitalWrite(verde, LOW);
-      digitalWrite(rojo, HIGH);
-      noTone(pinBuzzer);
-      Serial.println();
-      mfrc522.PICC_HaltA();
-    }
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
+    Serial.println("");
+    Serial.println("Connected");
+    Serial.print("Direccion IP: ");
+    Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+  }
+  
+  //look for new card
+   if ( ! mfrc522.PICC_IsNewCardPresent()) {
+  return;//got to start of loop if there is no card present
+ }
+ // Select one of the cards
+ if ( ! mfrc522.PICC_ReadCardSerial()) {
+  return;//if read card serial(0) returns 1, the uid struct contians the ID of the read card.
+ }
+
+ for (byte i = 0; i < mfrc522.uid.size; i++) {
+     CardID += mfrc522.uid.uidByte[i];
 }
+  
+  HTTPClient http;    //Declare object of class HTTPClient
+  
+  //GET Data
+  getData = "?CardID=" + CardID;  //Note "?" added at front
+  Link = "http://192.168.x.x/loT/intento/postdemo.php" + getData;
+  
+  http.begin(Link);
+  
+  int httpCode = http.GET();            //Send the request
+  delay(10);
+  String payload = http.getString();    //Get the response payload
+  
+  Serial.println(httpCode);   //Print HTTP return code
+  Serial.println(payload);    //Print request response payload
+  Serial.println(CardID);     //Print Card ID
+  
+  if(payload == "login"){
+    digitalWrite(RedLed,HIGH);
+    Serial.println("rojo encendido");
+    delay(500);  //Post Data at every 5 seconds
+  }
+  else if(payload == "salir"){
+    digitalWrite(BlueLed,HIGH);
+    Serial.println("verde encendido");
+    delay(500);  //Post Data at every 5 seconds
+  }
+  else if(payload == "exitosa" || payload == "tarjeta valida"){
+    digitalWrite(BlueLed,HIGH);
+    digitalWrite(RedLed,HIGH);
+    delay(500);  
+  }
+  delay(500);
+  
+  CardID = "";
+  getData = "";
+  Link = "";
+  http.end();  //Close connection
+  
+  digitalWrite(RedLed,LOW);
+  digitalWrite(BlueLed,LOW);
+
+  //tone(pinBuzzer, b[5], 1000);
+  //noTone(pinBuzzer);//
+
+}
+      
